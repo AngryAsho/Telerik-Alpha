e1c9393d0b53db9746362bfeef4fed3c4dcf09d1
Unknown <angryasho@gmail.com>
2017-11-25 16:34:35 +0200

    EX01 and EX02 added

---
+++ EX01FillTheMatrix/MatrixFiller.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace EX01FillTheMatrix
+{
+    class MatrixFiller
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            char leter = char.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int num = 1;
+
+            if (leter == 'a')
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    for (int row = 0; row < n; row++)
+                    {
+                        matrix[row, col] = num++;
+                    }
+                }
+            }
+            else if (leter == 'b')
+            {
+                bool down = true;
+                for (int col = 0; col < n; col++)
+                {
+                    if (down)
+                    {
+                        for (int row = 0; row < n; row++)
+                        {
+                            matrix[row, col] = num++;
+                        }
+                        down = false;
+                    }
+                    else
+                    {
+                        for (int row = n - 1; row >= 0; row--)
+                        {
+                            matrix[row, col] = num++;
+                        }
+                        down = true;
+                    }
+                }
+            }
+            else if (leter == 'c')
+            {
+                for (int row = n - 1, col = 0; col < n;)
+                {
+                    matrix[row, col] = num++;
+
+                    for (int startR = row, startC = col; ;)
+                    {
+                        if (((startR + 1) < n) && ((startC + 1) < n))
+                        {
+                            startR++;
+                            startC++;
+                            matrix[startR, startC] = num++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (row != 0)
+                    {
+                        row--;
+                    }
+                    else
+                    {
+                        col++;
+                    }
+                }
+            }
+            else if (leter == 'd')
+            {
+                char dir = 'd';
+                for (int col = 0, row = 0; num <= (n * n);)
+                {
+                    matrix[row, col] = num++;
+
+                    switch (dir)
+                    {
+                        case 'd':
+                            if (((row + 1) == n) || (matrix[row + 1, col] != 0))
+                            {
+                                dir = 'r';
+                                col++;
+                            }
+                            else
+                            {
+                                row++;
+                            }
+                            break;
+                        case 'r':
+                            if (((col + 1) == n) || (matrix[row, col + 1] != 0))
+                            {
+                                dir = 'u';
+                                row--;
+                            }
+                            else
+                            {
+                                col++;
+                            }
+                            break;
+                        case 'u':
+                            if (((row - 1) == -1) || (matrix[row - 1, col] != 0))
+                            {
+                                dir = 'l';
+                                col--;
+                            }
+                            else
+                            {
+                                row--;
+                            }
+                            break;
+                        case 'l':
+                            if (((col - 1) == -1) || (matrix[row, col - 1] != 0))
+                            {
+                                dir = 'd';
+                                row++;
+                            }
+                            else
+                            {
+                                col--;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (col == n - 1)
+                    {
+                        Console.Write("{0}", matrix[row, col]);
+                        break;
+                    }
+
+                    Console.Write("{0} ", matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EX02MaximalSum/SumCollector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EX02MaximalSum
+{
+    class SumCollector
+    {
+        static void Main()
+        {
+            // Parsing from string input with spaces to an int array
+            string[] NM = Console.ReadLine().Split(' ');
+            int N = int.Parse(NM[0]);
+            int M = int.Parse(NM[1]);
+            int[,] matrix = new int[N, M];
+
+            for (int row = 0; row < N; row++)
+            {
+                string matrixInput = Console.ReadLine();
+                string[] arrLine = new string[M];
+                arrLine = matrixInput.Split(' ');
+
+                for (int col = 0; col < M; col++)
+                {
+                    matrix[row, col] = int.Parse(arrLine[col]);
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int currentSum = int.MinValue;
+
+            for (int row = 0; row < N - 2; row++)
+            {
+                for (int col = 0; col < M - 2; col++)
+                {
+                    currentSum = matrix[row, col] + matrix[row + 1, col] + matrix[row + 2, col] + matrix[row, col + 1] + matrix[row, col + 2] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                    }
+                }
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
